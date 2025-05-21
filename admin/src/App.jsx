1ce3c1703a3e435edbdcc025e1f775b0a7551b49
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-22 00:44:53 +0600

    2

---
+++ admin/src/App.jsx
@@ -0,0 +1,41 @@
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import { useEffect, useState } from 'react'
+import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
+
+export const backendUrl = "http://localhost:3000";
+export const currency = "$";
+
+console.log("Backend URL:", backendUrl);
+export default function App() {
+  const [token, setToken] = useState(localStorage.getItem("token") || "")
+  useEffect(() => {
+    localStorage.setItem("token", token)
+  }, [token])
+  return (
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer />
+      {token === "" ? <Login setToken={setToken} /> :
+        <>
+          <Navbar setToken={setToken} />
+          <hr className='border-gray-300 mt-3' />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add token={token} />} />
+                <Route path='/list' element={<List token={token} />} />
+                <Route path='/orders' element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
+    </div>
+  )
+}
