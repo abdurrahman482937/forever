ea67703e0fce7389b3df63771e630d575f285a6a
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-22 19:37:09 +0600

    fix same

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const adminAuth = async (req, res, next) => {
     try {
-        const { token } = req.headers;
+        const token = req.headers.authorization?.split(" ")[1];
 
         if (!token) {
             return res.json({ success: false, message: "Not Authorized, Login Again" });
