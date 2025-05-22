47415edb74b7137e663662a6fe41f0dbe04885aa
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-22 15:23:24 +0600

    Done..! Go to the live show

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,8 @@ import { useEffect, useState } from 'react'
 import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 
-export const backendUrl = "http://localhost:3000";
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 export const currency = "$";
 
 console.log("Backend URL:", backendUrl);
